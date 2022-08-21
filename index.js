a33019ee537c7e3088960a29192604732afeb4ec
dennis00095 <dennis.mwongela@student.moringaschool.com>
2022-08-21 22:35:26 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Mwongela Dennis';
+
+//const welcomeMessage = 'Welcome to Flatbook, ';
+
+//const welcomeMessage = currentUser;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
